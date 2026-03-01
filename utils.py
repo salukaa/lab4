3abb6bf06f2490dc5a61bdad6e71a1e93caf8253
botuser228 <krashanytsa0@gmail.com>
2026-03-01 22:36:13 +0200

    add my function

--- utils.py
+++ utils.py
@@ -5,3 +5,11 @@ def factorial(n):
     for i in range(2, n + 1):
         result = result * i
     return result
+
+
+def gcd(a, b):
+    while b > 0:
+        c = a % b
+        a = b
+        b = c
+    return a
